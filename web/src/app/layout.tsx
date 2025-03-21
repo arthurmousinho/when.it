ef9b36641abc44c658e91c7e1f209b0c619b5bb7
arthurmousinho <arthursilvamo296@gmail.com>
2025-03-21 20:21:35 -0300

    :tada: initial commit

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import faviconImage from "@/assets/icon-logo.svg"
+
+export const metadata: Metadata = {
+  title: "when.it",
+  description: "An educational platform designed to connect students with their school's knowledge base through an AI-powered chatbot",
+  icons: {
+    icon: faviconImage.src,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
