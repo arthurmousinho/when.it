0e6a9f385c88c2567442e1561f45b612de4d04cb
arthurmousinho <arthursilvamo296@gmail.com>
2025-04-01 13:29:34 -0300

    feat: add authentication check and redirect in AuthLayout

--- web/src/app/auth/layout.tsx
+++ web/src/app/auth/layout.tsx
@@ -1,10 +1,19 @@
 import { Logo } from "@/components/logo"
+import { isAuthenticated } from "./(auth)/auth";
+import { redirect } from "next/navigation";
 
 type AuthLayoutProps = {
     children: React.ReactNode
 }
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default async function AuthLayout({ children }: AuthLayoutProps) {
+
+    const isUserAuthenticated = await isAuthenticated();
+
+    if (isUserAuthenticated) {
+        redirect('/orgs');
+    }
+
     return (
         <div className="h-dvh w-full flex items-center justify-center">
             <div className="fixed left-10 top-10">
